18a233d90c4561a219b4bfdcb1a634e08e789be9
AYMEN BRAIEK <05234@biat.com.tn>
2020-07-16 22:58:20 +0100

    conf dynamic  -closes #1

--- src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,14 +15,14 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-	   @Bean
+	  // @Bean
 		//configurer  spring cloud gateway statiquement pour les API public
 		RouteLocator StaticRoutes(RouteLocatorBuilder builder)
 	{
 		return builder.routes()
-				.route(r->r.path("/customers/**").uri("http://localhost:8181/").id("r1"))
-				.route(r->r.path("/products/**").uri("https://localhost:8282/").id("r2"))
-				.route(r->r.path("/biat/**").uri("lb://CUSTOMER-SERVICE").id("r3"))
+				.route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE").id("r1"))
+				.route(r->r.path("/products/**").uri("lb://INVENTORY-SERVICE").id("r2"))
+				//.route(r->r.path("/biat/**").uri("lb://CUSTOMER-SERVICE").id("r3"))
 				/*.route(r->r.path("/publicscountries/**").filters(f->f.addRequestHeader("x-rapidapi-host","restcountries-v1.p.rapidapi.com")
 				.addRequestHeader("x-rapidapi-key","65638b4164msh211250a475b1409p17b586jsn8e1f2ffc3370")
 				.rewritePath("/publicscountries/(?<segment>.*)","/${segment}")
@@ -34,5 +37,13 @@ public class GatewayServiceApplication {
 						.uri( "https://muslimsalat.p.rapidapi.com").id("route2"))*/
 				.build();
 	}
+		
+		//configurer spring cloud gateway dynamiquement
+		@Bean
+		DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp)
+		{
+			return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+		}
+
 
 }
